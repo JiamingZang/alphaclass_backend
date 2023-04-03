3c5fd2b1b077febc731bf6913f719a5a25122921
JM Zang <1831544918@qq.com>
2023-04-03 22:17:08 +0800

    translation and wiki service added

---
+++ src/main/java/com/imct/alphaclass/controller/TranslationController.java
@@ -0,0 +1,316 @@
+package com.imct.alphaclass.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
+import com.imct.alphaclass.common.JSONResult;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import org.apache.http.Header;
+import org.apache.http.HttpEntity;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+import org.apache.commons.text.StringEscapeUtils;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+
+@RestController
+public class TranslationController {
+
+    @RequestMapping(value = "/services/test", method = RequestMethod.GET)
+    public JSONResult getDataFromUrl(@RequestParam(name = "word",required = true) String word) throws IOException{
+        return JSONResult.successWithData(TranslateByYouDao(word));
+    }
+
+    @RequestMapping(value = "/services/testword", method = RequestMethod.GET)
+    public JSONResult getDataFromUrl1(@RequestParam(name = "word",required = true) String word) throws IOException{
+        return JSONResult.successWithData(translateEN(word));
+    }
+
+        
+    public List<YoudaoTranslationResult> TranslateByYouDao(String q) throws IOException{
+        List<YoudaoTranslationResult> enResults = new ArrayList<YoudaoTranslationResult>();
+        String url = "https://openapi.youdao.com/api";
+        Map<String, String> dic = getRequestMap(q, "zh-CHS", "en");//所有上传的参数将写入该字典中
+        YoudaoTranslationResult wordResult = requestForHttp(url, dic);
+
+        dic.put("from", "en");
+        dic.put("to", "zh-CHS");
+
+        if (wordResult.basic != null) {
+            for (String enq : wordResult.basic.explains) {
+                dic = getRequestMap(enq, "en", "zh-CHS");
+                YoudaoTranslationResult enResult = requestForHttp(url, dic);
+                enResult.exampleSentences = getExampleSentences(enq);
+                enResults.add(enResult);
+            }
+        }
+        return enResults;
+    }
+    public YoudaoTranslationResult translateCN(String q){
+        Map<String, String> dic = getRequestMap(q, "zh-CHS", "en");//所有上传的参数将写入该字典中
+        String url = "https://openapi.youdao.com/api";
+        return get(url, dic);
+
+    }
+
+    public YoudaoTranslationResult translateEN(String q){
+        Map<String, String> dic = getRequestMap(q, "en", "zh-CHS");//所有上传的参数将写入该字典中
+        String url = "https://openapi.youdao.com/api";
+        YoudaoTranslationResult res = get(url, dic);
+        res.exampleSentences = getExampleSentences(q);
+        return res;
+        
+    }
+
+    public class WebResult {
+        public List<String> value;
+        public String key;
+    }
+
+    public class BasicResult {
+        public String phonetic;
+        public List<String> explains;
+    }
+
+    public class YoudaoTranslationResult {
+        public String errorCode;
+        public String query;
+        public List<String> translation;
+        public BasicResult basic;
+        public List<WebResult> web;
+        public String l;
+        public String tSpeakUrl;
+        public String speakUrl;
+        public List<String> returnPhrase;
+        public List<ExampleSentencesResult> exampleSentences;
+    }
+
+    public class ExampleSentencesResult {
+        public String enExampleSentence;
+        public String cnExampleSentence;
+        public ExampleSentencesResult(String enExampleSentence, String cnExampleSentence) {
+            this.enExampleSentence = enExampleSentence;
+            this.cnExampleSentence = cnExampleSentence;
+        }
+        
+    }
+
+    private static final String APP_KEY = "REPLACED_YOUDAO_APP_KEY";
+
+    private static final String APP_SECRET = "REPLACED_YOUDAO_APP_SECRET";
+
+    public Map<String,String> getRequestMap(String q,String from,String to) {
+        Map<String,String> params = new HashMap<String,String>();
+        String salt = String.valueOf(System.currentTimeMillis());
+        params.put("from", from);
+        params.put("to", to);
+        params.put("signType", "v3");
+        String curtime = String.valueOf(System.currentTimeMillis() / 1000);
+        params.put("curtime", curtime);
+        String signStr = APP_KEY + truncate(q) + salt + curtime + APP_SECRET;
+        String sign = getDigest(signStr);
+        params.put("appKey", APP_KEY);
+        params.put("q", q);
+        params.put("salt", salt);
+        params.put("sign", sign);
+        return params;
+        // /** 处理结果 */
+        // requestForHttp(YOUDAO_URL,params);
+    }
+
+    public YoudaoTranslationResult requestForHttp(String url,Map<String,String> params) throws IOException {
+
+        /** 创建HttpClient */
+        CloseableHttpClient httpClient = HttpClients.createDefault();
+
+        /** httpPost */
+        HttpPost httpPost = new HttpPost(url);
+        List<NameValuePair> paramsList = new ArrayList<NameValuePair>();
+        Iterator<Map.Entry<String,String>> it = params.entrySet().iterator();
+        while(it.hasNext()){
+            Map.Entry<String,String> en = it.next();
+            String key = en.getKey();
+            String value = en.getValue();
+            paramsList.add(new BasicNameValuePair(key,value));
+        }
+        httpPost.setEntity(new UrlEncodedFormEntity(paramsList,"UTF-8"));
+        CloseableHttpResponse httpResponse = httpClient.execute(httpPost);
+        
+        Header[] contentType = httpResponse.getHeaders("Content-Type");
+        // logger.info("Content-Type:" + contentType[0].getValue());
+        String json = "";
+        if("audio/mp3".equals(contentType[0].getValue())){
+            //如果响应是wav
+            HttpEntity httpEntity = httpResponse.getEntity();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            httpResponse.getEntity().writeTo(baos);
+            byte[] result = baos.toByteArray();
+            EntityUtils.consume(httpEntity);
+            if(result != null){//合成成功
+                String file = "合成的音频存储路径"+System.currentTimeMillis() + ".mp3";
+                byte2File(result,file);
+            }
+        }else{
+            /** 响应不是音频流，直接显示结果 */
+            HttpEntity httpEntity = httpResponse.getEntity();
+            json = EntityUtils.toString(httpEntity,"UTF-8");
+            EntityUtils.consume(httpEntity);
+            // logger.info(json);
+            // System.out.println(json);
+        }
+        // System.out.println(StringEscapeUtils.unescapeJson(json));
+        return JSON.parseObject(StringEscapeUtils.unescapeJson(json),new TypeReference<YoudaoTranslationResult>(){});
+
+        
+    }
+
+    /**
+     * 生成加密字段
+     */
+    public String getDigest(String string) {
+        if (string == null) {
+            return null;
+        }
+        char hexDigits[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        byte[] btInput = string.getBytes(StandardCharsets.UTF_8);
+        try {
+            MessageDigest mdInst = MessageDigest.getInstance("SHA-256");
+            mdInst.update(btInput);
+            byte[] md = mdInst.digest();
+            int j = md.length;
+            char str[] = new char[j * 2];
+            int k = 0;
+            for (byte byte0 : md) {
+                str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+                str[k++] = hexDigits[byte0 & 0xf];
+            }
+            return new String(str);
+        } catch (NoSuchAlgorithmException e) {
+            return null;
+        }
+    }
+
+    /**
+    *
+    * @param result 音频字节流
+    * @param file 存储路径
+    */
+    private void byte2File(byte[] result, String file) {
+        File audioFile = new File(file);
+        FileOutputStream fos = null;
+        try{
+            fos = new FileOutputStream(audioFile);
+            fos.write(result);
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            if(fos != null){
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+    }
+
+    public String truncate(String q) {
+        if (q == null) {
+            return null;
+        }
+        int len = q.length();
+        return len <= 20 ? q : (q.substring(0, 10) + len + q.substring(len - 10, len));
+    }
+
+    
+    public List<ExampleSentencesResult> getExampleSentences(String enq){
+        List<ExampleSentencesResult> res = new ArrayList<ExampleSentencesResult>();
+        String wordURL = "https://www.youdao.com/result?word=" + enq + "&lang=en";
+        OkHttpClient client = new OkHttpClient().newBuilder()
+        .build();
+        Request request = new Request.Builder()
+        .url(wordURL)
+        .method("GET", null)
+        .build();
+        try {
+            Response response = client.newCall(request).execute();
+            Document document = Jsoup.parse(response.body().string());
+            // System.out.println(document.title());
+            document.select("script").remove();
+            document.select("link").remove();
+            document.select("style").remove();
+            Elements enHtmlNodes = document.getElementsByClass("sen-eng");
+            Elements cnHtmlNodes = document.getElementsByClass("sen-ch");
+            
+            if (enHtmlNodes!=null && cnHtmlNodes!=null) {
+                for (int i = 0; i < enHtmlNodes.size(); i++) {
+                    ExampleSentencesResult result = 
+                    new ExampleSentencesResult(enHtmlNodes.get(i).text(),cnHtmlNodes.get(i).text());
+                    res.add(result);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+
+
+    public YoudaoTranslationResult get(String url, Map<String,String> map){
+        StringBuilder builder = new StringBuilder();
+
+        int i = 0;
+        for ( Map.Entry<String,String> entry : map.entrySet()) {
+            if (i>0) {
+                builder.append("&");
+            }
+            builder.append(entry.getKey()+"="+entry.getValue());
+            i++;
+        }
+
+        System.out.println(url+"?"+builder.toString());
+        OkHttpClient client = new OkHttpClient().newBuilder()
+            .build();
+        Request request = new Request.Builder()
+            .url(url+"?"+builder.toString())
+            .method("GET", null)
+            .build();
+        try {
+            Response response = client.newCall(request).execute();
+            String res = response.body().string();
+            return JSON.parseObject(res,new TypeReference<YoudaoTranslationResult>(){});
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
+    }
+}
---
+++ src/main/java/com/imct/alphaclass/controller/WikiController.java
@@ -0,0 +1,193 @@
+package com.imct.alphaclass.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.imct.alphaclass.common.JSONResult;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+
+@RestController
+public class WikiController {
+
+    public class WikiResult {
+        private String keyword;
+        private String short_description;
+        private String url;
+        private String long_description;
+        public WikiResult(String keyword, String short_description, String url, String long_description) {
+            this.keyword = keyword;
+            this.short_description = short_description;
+            this.url = url;
+            this.long_description = long_description;
+        }
+        public String getKeyword() {
+            return keyword;
+        }
+        public void setKeyword(String keyword) {
+            this.keyword = keyword;
+        }
+        public String getShort_description() {
+            return short_description;
+        }
+        public void setShort_description(String short_description) {
+            this.short_description = short_description;
+        }
+        public String getUrl() {
+            return url;
+        }
+        public void setUrl(String url) {
+            this.url = url;
+        }
+        public String getLong_description() {
+            return long_description;
+        }
+        public void setLong_description(String long_description) {
+            this.long_description = long_description;
+        }
+        
+        
+    }
+
+    @RequestMapping(value = "/services/get-proxy", method = RequestMethod.GET)
+    public String getDataFromUrl(@RequestParam(name = "url",required = true) String urlPrarm){
+        OkHttpClient client = new OkHttpClient().newBuilder()
+            .build();
+        // MediaType mediaType = MediaType.parse("text/plain");
+        // RequestBody body = RequestBody.create(mediaType, "");
+        Request request = new Request.Builder()
+            .url(urlPrarm)
+            .method("GET", null)
+            .addHeader("Accept", "text/plain")
+            // .addHeader("Cookie", "BAIDUID=6E36BC3EA538530148FB819204DCDEF3:FG=1; X_ST_FLOW=0")
+            .build();
+        try {
+            Response response = client.newCall(request).execute();
+            Document document = Jsoup.parse(response.body().string());
+                // System.out.println(document.title());
+            document.select("script").remove();
+            document.select("link").remove();
+            document.select("style").remove();
+            return document.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return e.toString();
+        }
+    }
+
+    @RequestMapping(value = "/services/get-wiki-items", method = RequestMethod.GET)
+    public JSONResult getWikiItems(@RequestParam(name = "keyword",required = true) String keyword){
+        OkHttpClient client = new OkHttpClient().newBuilder()
+            .build();
+        
+        String uri = "http://baike.baidu.com/search/word?word=" + keyword;
+        Request request = new Request.Builder()
+            .url(uri)
+            .method("GET", null)
+            .addHeader("Accept", "text/plain")
+            .build();
+        try {
+            Response response = client.newCall(request).execute();
+            Document document = Jsoup.parse(response.body().string());
+            document.select("script").remove();
+            document.select("link").remove();
+            document.select("style").remove();
+
+            List<WikiResult> results = new ArrayList<WikiResult>();
+
+             //对于无歧义词或有常见解释的多义词（如苹果）可以直接获取百科解释desrciption
+            Element kwbox = document.select("meta[name=description]").first();
+            String description = kwbox.attr("content");
+
+            // 该关键词无解释
+            if (description == "百度百科是一部内容开放、自由的网络百科全书，旨在创造一个涵盖所有领域知识，服务所有互联网用户的中文知识性百科全书。在这里你可以参与词条编辑，分享贡献你的知识。")
+            {
+
+                return JSONResult.successWithData(results);
+            }
+
+            // 判断是否为有常见解释的多义词，如“苹果”
+            String href = "";
+            Elements is_polysemant = document.getElementsByClass("polysemantList-wrapper cmn-clearfix");
+            if (!is_polysemant.isEmpty()) {
+                Elements polysemant_list = is_polysemant.select("li");
+                for (Element item : polysemant_list) {
+                    Element aNode = item.select("a").first();
+                    if (aNode!=null) {
+                        href =  "https://baike.baidu.com" + item.select("a").attr("href");
+                        results.add(new WikiResult(keyword,item.select("a").attr("title"),href,""));
+                    }else{
+                        Element spanNode = item.select("span").first();
+                        results.add(new WikiResult(keyword,spanNode.text(),"",description));
+                    }
+                }
+                return JSONResult.successWithData(results);
+            }
+
+            // 判断是否为无常见解释的多义词，如“递归”
+            Elements is_polysemant2 = document.getElementsByClass("lemmaWgt-subLemmaListTitle");
+            if (!is_polysemant2.isEmpty()) {
+                Element poly_node2 = document.getElementsByClass("custom_dot para-list list-paddingleft-1").first();
+                Elements nodes = poly_node2.select("a");
+                for (Element node : nodes) {
+                    href = "https://baike.baidu.com"+node.attr("href");
+                    // 将该义项加入候选列表
+                    results.add(new WikiResult(keyword, node.text(), href, ""));
+                }
+                return JSONResult.successWithData(results);
+            }
+
+            // 该词为无歧义词
+            results.add(new WikiResult(keyword, keyword, "", description));
+            return JSONResult.successWithData(results);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return JSONResult.failWithMsg("400", e.toString());
+        }
+    }
+
+    @RequestMapping(value = "/services/get-long-description", method = RequestMethod.GET)
+    public JSONResult getLongDescription(@RequestParam(name = "uri",required = true) String uri){
+        OkHttpClient client = new OkHttpClient().newBuilder()
+            .build();
+        String description = "";
+        Request request = new Request.Builder()
+            .url(uri)
+            .method("GET", null)
+            .addHeader("Accept", "text/plain")
+            .build();
+        try {
+
+            Response response = client.newCall(request).execute();
+            Document document = Jsoup.parse(response.body().string());
+            document.select("script").remove();
+            document.select("link").remove();
+            document.select("style").remove();
+
+            Element kwbox = document.select("meta[name=description]").first();
+            description = kwbox.attr("content");
+            String keyword = document.getElementsByClass("lemmaWgt-lemmaTitle-title J-lemma-title").first().select("h1").first().text();
+            Map<String,Object> result = new HashMap<String,Object>();
+            result.put("keyword", keyword);
+            result.put("long_description", description);
+            return JSONResult.successWithData(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return JSONResult.failWithMsg("400", e.toString());
+        }
+    }
+}
